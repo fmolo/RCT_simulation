790bf627616327c910e73dd9c2f600f56a0276d6
Fabio Molo <fabio.molo@gmail.com>
2024-12-29 09:54:35 +0100

    improve study selection from clinicaltrials.gov

--- get_clinicaltrials_gov_data.R
+++ get_clinicaltrials_gov_data.R
@@ -1,24 +1,32 @@
 library(ctrialsgov)
 
-# loads all the data from clinicaltrials.gov into a cache (ca. 500MB)
-# this takes time 
+# load all the data from clinicaltrials.gov into a cache (ca. 500MB)
+# this takes some time 
 ctgov_load_cache()
 
-# query all phase 3 studies from the cached data
+# query all phase 2 and 3 randomized studies with parallel assignment from the cache
 data_load <- ctgov_query(
-    phase = "Phase 3",
+    phase = c("Phase 2", "Phase 3"),
     allocation = "Randomized",
     intervention_model = "Parallel Assignment",
-    primary_purpose = "Treatment",
     )
 
+# select only completed studies
+data <- data[data$rec_status == "Completed", ]
 
-# only select studies with the completion_date year in 2023
+# only select studies with the completion_date between 2013 and 2023
 data <- data_load[data_load$completion_date >= "2013-01-01" & data_load$completion_date <= "2024-12-31", ]
 
 # remove studies with unknown number of participants
 data <- data[!is.na(data$enrollment), ]
 
-nrow(data) # number of studies
-median(data$enrollment) # median number of participants (if known)
+# get the total number of studies
+nrow(data) 
+
+# get the median number of participants
+median(data$enrollment) 
+
+# get the median number of participants per phase
+tapply(data$enrollment, data$phase, median) # median number of participants per phase
+
 
